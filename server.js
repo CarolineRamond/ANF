d75910f0e48c3193ee9b2759d19c463da6eb18d1
Caroline Ramond <caroline-ramond@orange.fr>
2016-11-29 17:49:20 +0100

    hello world complete with comments and absolute path

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+// dépendances
 var express = require('express');
+var path = require('path');
 
+// création du serveur
 var server = express();
 
-server.get('/', function (req, res) {
-	res.sendfile('index.html');
+// dossier statique (tous les fichiers du 
+// dossier seront chargés dans le navigateur)
+server.use(express.static('client'));
+
+
+// GET / : affichage de la page html
+server.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, 'client/index.html'));
+  // sendFile nécessite un chemin absolu
 });
 
+
+// écoute des requêtes sur le port 8000
 server.listen(8000, function () {
 	console.log('Server now listening on port 8000');
 });
