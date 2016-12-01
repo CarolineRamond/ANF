65324526a9909023a19dcdc0c1db69e7bfe6c2a8
Caroline Ramond <caroline-ramond@orange.fr>
2016-12-01 15:21:48 +0100

    STEP 4

---
+++ calcul.py
@@ -0,0 +1,21 @@
+from socketIO_client import SocketIO
+import numpy as np
+import numpy.random as npr
+
+
+class Main():
+    def __init__(self, parent = None):
+        self.socketIO = SocketIO('localhost', 8080)
+
+    def run(self):
+        while 1:
+            positions = npr.uniform(low=0.0,high=10.0,size=3)
+            buff = bytearray(positions.tobytes())
+            self.socketIO.emit('data', 
+                { "data" : buff });
+            self.socketIO.wait(seconds=0.1)
+
+
+if __name__ =="__main__":
+    m = Main()
+    m.run()
